c425a4c6a74e927413d1f5517135974be71a9c84
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2023-03-14 17:00:04 +0100

    feat: add phpcs

--- api/src/index.php
+++ api/src/index.php
@@ -1,21 +1,32 @@
 <?php
-  use Kesa\Sacculum\Core\Router;
-  use Dotenv\Dotenv;
+/**
+ * API entry point.
+ * php version 8.2
+ *
+ * @file
+ * @category Core
+ * @package  Kesa\Sacculum
+ * @author   Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
+ * @license  Not licensed
+ * @link     https://github.com/VTS-Kesa/sacculum
+ */
+use Kesa\Sacculum\Core\Router;
+use Dotenv\Dotenv;
 
-  // Load Composer's autoloader
-  require_once '../vendor/autoload.php';
+// Load Composer's autoloader
+require_once '../vendor/autoload.php';
 
-  // Load environment variables
-  Dotenv::createImmutable('../')->load();
+// Load environment variables
+Dotenv::createImmutable('../')->load();
 
   // Set error reporting
-  if ($_ENV['ENVIRONMENT'] !== 'prod') {
+if ($_ENV['ENVIRONMENT'] !== 'prod') {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-  }
-  
-  // Create a new router
-  $router = new Router();
-  // Run the router
-  $router->run();
+}
+    
+// Create a new router
+$router = new Router();
+// Run the router
+$router->run();
 ?>
--- api/tests/BasicTest.php
+++ api/tests/BasicTest.php
@@ -1,8 +1,35 @@
 <?php
+/**
+ * Basic test.
+ * php version 8.2
+ *
+ * @file
+ * @category Test
+ * @package  Kesa\Sacculum\Test
+ * @author   Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
+ * @license  Not licensed
+ * @link     https://github.com/VTS-Kesa/sacculum
+ */
 use PHPUnit\Framework\TestCase;
 
-final class BasicTest extends TestCase {
-    public function testBasic() {
+/**
+ * Basic test.
+ *
+ * @category Test
+ * @package  Kesa\Sacculum\Test
+ * @author   Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
+ * @license  Not licensed
+ * @link     https://github.com/VTS-Kesa/sacculum
+ */
+final class BasicTest extends TestCase
+{
+    /**
+     * Test if the test is working.
+     *
+     * @return void
+     */
+    public function testBasic()
+    {
         $this->assertTrue(true);
     }
 }
